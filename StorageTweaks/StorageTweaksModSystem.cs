43c5e7e60d26d51f02ebcd45a93fa692f9d6572d
Nick <270792863+trojan96g@users.noreply.github.com>
2026-05-07 11:21:11 -0600

    fixed compatibility with playerinventorylib without Backpacks mod

--- StorageTweaks/StorageTweaksModSystem.cs
+++ StorageTweaks/StorageTweaksModSystem.cs
@@ -52,7 +52,9 @@ public class StorageTweaksModSystem : ModSystem
             // for overhaullib before 1.22, Quivers And Sheaths and Backpacks mod use this slot type before 1.22
             "ItemSlotBagContentWithWildcardMatch",
             // for https://mods.vintagestory.at/playerinventorylib used by backpacks mod in 1.22+
-            "BackpackSlot"
+            "BackpackSlot",
+            // for https://mods.vintagestory.at/playerinventorylib without the Backpacks mod
+            "VanillaBagContentSlot"
         ];
 
     private static StorageTweaksClientConfig _config = new();
