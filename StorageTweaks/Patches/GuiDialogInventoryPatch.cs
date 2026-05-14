a62a69ec8197c7bf3125ca78db79db21e52f1f9d
Nick <270792863+trojan96g@users.noreply.github.com>
2026-05-14 13:25:00 -0600

    fixed crash when closing creative inventory

--- StorageTweaks/Patches/GuiDialogInventoryPatch.cs
+++ StorageTweaks/Patches/GuiDialogInventoryPatch.cs
@@ -17,11 +17,18 @@ public class GuiDialogInventoryPatch
     // ReSharper disable once InconsistentNaming
     public static void ComposeSurvivalInvDialog(GuiDialogInventory __instance)
     {
-        var composer =  GetComposer(__instance);
-        var capi = composer.Api!;
+        var composer = GetComposer(__instance);
+        var capi = GetApi(__instance);
+
+        if (composer == null)
+        {
+            capi.Logger.Warning("[StorageTweaks] StorageTweaksModSystem not found for inventory");
+            return;
+        }
+
         capi.Logger.Debug("[StorageTweaks] Composing inventory action buttons.");
-        
-        var modSystem = composer.Api.ModLoader.GetModSystem<StorageTweaksModSystem>();
+
+        var modSystem = capi.ModLoader.GetModSystem<StorageTweaksModSystem>();
 
         if (modSystem == null)
         {
@@ -31,23 +38,29 @@ public class GuiDialogInventoryPatch
 
         modSystem.InventoryActionButtons!.ComposeGui(composer);
     }
-    
+
     [HarmonyPatch(typeof(GuiDialogInventory), "OnGuiClosed")]
     [HarmonyPostfix]
     // ReSharper disable once InconsistentNaming
     public static void OnGuiClosed(GuiDialogInventory __instance)
     {
-        var composer =  GetComposer(__instance);
-        var modSystem = composer.Api.ModLoader.GetModSystem<StorageTweaksModSystem>();
-        if (modSystem.FavoritesManager == null) return;
+        var composer = GetComposer(__instance);
+        var modSystem = composer?.Api.ModLoader.GetModSystem<StorageTweaksModSystem>();
+        if (modSystem?.FavoritesManager == null) return;
         modSystem.FavoritesManager.IsFavoriteModeActive = false;
     }
 
-    private static GuiComposer GetComposer(GuiDialogInventory inventoryDialog)
+    private static GuiComposer? GetComposer(GuiDialogInventory inventoryDialog)
     {
-        
-        var field = inventoryDialog.GetType().GetField("survivalInvDialog", BindingFlags.NonPublic | BindingFlags.Instance)!;
-        var composer = (GuiComposer)field.GetValue(inventoryDialog)!;
+        var field = inventoryDialog.GetType()
+            .GetField("survivalInvDialog", BindingFlags.NonPublic | BindingFlags.Instance)!;
+        var composer = (GuiComposer?)field.GetValue(inventoryDialog);
         return composer;
+    }
+
+    private static ICoreClientAPI GetApi(GuiDialogInventory inventoryDialog)
+    {
+        var field = inventoryDialog.GetType().GetField("capi",  BindingFlags.NonPublic | BindingFlags.Instance)!;
+        return (ICoreClientAPI)field.GetValue(inventoryDialog)!;
     }
 }
