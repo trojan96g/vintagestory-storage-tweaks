942d2a87e5d982cd47678a38e3ec229070707cf1
Nick <270792863+trojan96g@users.noreply.github.com>
2026-06-09 12:11:12 -0600

    added support for aged baskets, aged chest, crates, String Sense mod, Better Crates mod, Extra Chests mod, and Containers Bundle mod
    closes #12

--- StorageTweaks/QuickStoreNearbyContainerSystem.cs
+++ StorageTweaks/QuickStoreNearbyContainerSystem.cs
@@ -14,25 +14,87 @@ public static class QuickStoreNearbyContainerSystem
     // Chest types to include when looking for nearby chests to quick deposit matching items from inventory
     private static readonly HashSet<(string, string)> QuickStackChestTypes =
     [
+        // vanilla baskets
         ("basket", "reed"),
         ("basket", "papyrus"),
+        ("basket", "aged"),
+
+        // vanilla chests
         ("chest", "normal-labeled"),
         ("chest", "normal-generic"),
-        ("chest", "normal")
+        ("chest", "normal"),
+        ("chest", "normal-aged"),
+
+        // vanilla crates
+        ("crate", "crate"),
+
+        // String Sense mod containers
+        ("basket", "bark"),
+        ("basket", "vine"),
+        ("basket", "mixed"),
+        ("basket", "flax"),
+        ("basket", "straw"),
+
+        // Better Crates mod containers
+        ("bettercrate", "bettercrate2sided"),
+        ("bettercrate", "bettercrate"),
+
+        // Extra Chests mod
+        ("chest", "blackbronze"),
+        ("chest", "iron"),
+        ("chest", "bismuthbronze"),
+        ("chest", "steel"),
+        ("chest", "tinbronze"),
+        ("chest", "copper"),
+
+        // Containers Bundle mod
+        ("chest", "strongbox"),
+        ("chest", "metalcabinetnolabel"),
+        ("chest", "bamboochest"),
+        ("chest", "cupboardnolabel"),
+        ("chest", "stonecasket"),
+        ("chest", "cupboardwithlabel"),
+        ("chest", "woodenbox"),
+        ("chest", "exquisitechest"),
+        ("chest", "wickerbasket"),
+        ("chest", "foodcupboard"),
+        ("chest", "longcrate"),
+        ("chest", "linencrate"),
+        ("chest", "foodcupboardwall"),
     ];
 
-    private static List<BlockEntityGenericTypedContainer> GetNearbyContainers(IWorldAccessor world, BlockPos position,
+    private static List<BlockEntityContainer> GetNearbyContainers(IWorldAccessor world, BlockPos position,
         int radius)
     {
         var minPos = position - radius;
         var maxPos = position + radius;
-        var nearbyContainers = new List<BlockEntityGenericTypedContainer>();
+        var nearbyContainers = new List<BlockEntityContainer>();
         world.BlockAccessor.WalkBlocks(minPos, maxPos, (_, x, y, z) =>
         {
             var be = world.BlockAccessor.GetBlockEntity(new BlockPos(x, y, z));
-            if (be is not BlockEntityGenericTypedContainer container) return;
-            if (!QuickStackChestTypes.Contains((container.InventoryClassName, container.type))) return;
-            nearbyContainers.Add(container);
+
+            if (be is BlockEntityGenericTypedContainer container)
+            {
+                if (!QuickStackChestTypes.Contains((container.InventoryClassName, container.type)))
+                {
+                    world.Logger.Debug(
+                        $"[StorageTweaks] Skipped typed container for quick store nearby since it is not in the white list: (\"{container.inventoryClassName}\", \"{container.type}\")");
+                    return;
+                }
+
+                nearbyContainers.Add(container);
+            }
+
+            if (be is not BlockEntityContainer bc) return;
+            if (!QuickStackChestTypes.Contains((bc.InventoryClassName,
+                    bc.Block.Code.FirstCodePart())))
+            {
+                world.Logger.Debug(
+                    $"[StorageTweaks] Skipped container for quick store nearby since it is not in the white list: (\"{bc.InventoryClassName}\", \"{bc.Block.Code.FirstCodePart()}\"),");
+                return;
+            }
+
+            nearbyContainers.Add(bc);
         }, true);
 
         return nearbyContainers;
